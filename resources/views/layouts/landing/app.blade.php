e25b1f8266e7da44f51ffd42c28b59d1c9c621dd
Mohammed Sirhy <56832505+m0sirhy@users.noreply.github.com>
2020-05-06 18:03:53 +0300

    Update app.blade.php

--- resources/views/layouts/landing/app.blade.php
+++ resources/views/layouts/landing/app.blade.php
@@ -16,7 +16,20 @@
       @include('includes.landing.footer')
     </footer>
     <!-- oppi_footer_wrap -->
-
+<script>
+      (function(d,t) {
+        var BASE_URL = "https://app.chatwoot.com";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src= BASE_URL + "/packs/js/sdk.js";
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSDK.run({
+            websiteToken: 'UxE8wLCp4RBqkxFNm1uTEEag',
+            baseUrl: BASE_URL
+          })
+        }
+      })(document,"script");
+    </script>
 
     <script src="{{asset('landing/js/jquery-3.4.1.min.js')}}"></script>
     <script src="{{asset('landing/js/bootstrap.min.js')}}"></script>
